6e7a567a3880b8e4d2585ddc4f0672a2bf8bb6db
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-28 15:05:01 +0300

    Configuring Apollo

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,11 @@
-console.log('👻');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+);
