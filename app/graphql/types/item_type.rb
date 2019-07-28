990e1c381697717afb1bb93a39069ed569d0d639
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-28 13:10:41 +0300

    Add user type class (#5)

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -6,5 +6,6 @@ module Types
     field :title, String, null: true
     field :description, String, null: true
     field :image_url, String, null: true
+    field :user, Types::UserType, null: false
   end
 end
