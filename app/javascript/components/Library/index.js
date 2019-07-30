eb9f06fdf9f5b6fa9c9543bb1a839cb6848656b0
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-30 19:54:53 +0300

    Complete update items feature

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import { LibraryQuery } from './operations.graphql';
+import cs from './styles';
+import UpdateItemForm from '../UpdateItemForm';
 
-const LibraryQuery = gql`
-  {
-    items {
-      id
-      title
-      user {
-        email
-      }
-    }
-  }
-`;
+const Library = () => {
+  const [item, setItem] = useState(null);
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? 'loading...'
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+                <button
+                  key={id}
+                  className={cs.plate}
+                  onClick={() => setItem({ title, imageUrl, id, description })}
+                >
+                  <div className={cs.title}>{title}</div>
+                  <div>{description}</div>
+                  {imageUrl && <img src={imageUrl} className={cs.image} />}
+                  {user ? (
+                    <div className={cs.user}>added by {user.email}</div>
+                  ) : null}
+                </button>
+              ))}
 
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? 'loading...'
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
-            ))}
-      </div>
-    )}
-  </Query>
-);
+          {item !== null && (
+            <UpdateItemForm
+              id={item.id}
+              initialTitle={item.title}
+              initialDescription={item.description}
+              initialImageUrl={item.imageUrl}
+              onClose={() => setItem(null)}
+            />
+          )}
+        </div>
+      )}
+    </Query>
+  );
+};
+
+export default Library;
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library';
 import UserInfo from "../components/UserInfo";
+import AddItemForm from "../components/AddItemForm";
 
 render(
   <Provider>
     <UserInfo />
+    <AddItemForm />
     <Library />
   </Provider>,
   document.querySelector('#root')
