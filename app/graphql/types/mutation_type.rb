6608a16e9a1cbbbd058b3e988f99b901a30abc03
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-30 20:29:49 +0300

    Bugfix/Add update item mutation (#11)
    * Add update_item mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :sign_in, mutation: Mutations::SignInMutation
     field :add_item, mutation: Mutations::AddItemMutation
+    field :update_item, mutation: Mutations::UpdateItemMutation
   end
 end
