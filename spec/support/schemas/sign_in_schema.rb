856079d96515350d02b78cbaf46caf939bd118f3
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-30 22:55:21 +0300

    Implement match_schema rspec matcher, SignIn mutation schemas

---
+++ spec/support/schemas/sign_in_schema.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SignInSchema
+  Success = Dry::Schema.Params do
+    required(:data).value(:hash?).schema do
+      required(:signIn).value(:hash?).schema do
+        required(:token).value(:str?)
+        required(:user).value(:hash?).schema do
+          required(:id).value(:integer)
+          required(:fullName).value(:str?)
+        end
+      end
+    end
+  end
+
+  UserNotFound = Dry::Schema.Params do
+    required(:data).value(:hash?).schema do
+      required(:signIn).value(:none?)
+    end
+    required(:errors).value(:array).each do
+      schema do
+        required(:message).value(:string)
+        required(:path).value(:array).each(:str?)
+      end
+    end
+  end
+end
